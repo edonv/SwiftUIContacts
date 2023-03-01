9489001f26dd5dd6e5857eb415c3fd79e8cc0e61
Edon <edon@valdman.works>
2023-03-01 14:50:53 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUIContactsUI
+
+A description of this package.
---
+++ Sources/SwiftUIContactsUI/SwiftUIContactsUI.swift
@@ -0,0 +1,6 @@
+public struct SwiftUIContactsUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
