e2b401119249baf743311984ab89df2d75b19d6f
Edon <edon@valdman.works>
2023-03-01 17:19:35 -0500

    Renamed folder and package to SwiftUIContacts
    Decided that SwiftUIContactsUI was too clunky

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUIContactsUI",
+    name: "SwiftUIContacts",
     platforms: [
         .iOS(.v13),
         .macCatalyst(.v13)
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftUIContactsUI",
-            targets: ["SwiftUIContactsUI"]),
+            name: "SwiftUIContacts",
+            targets: ["SwiftUIContacts"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftUIContactsUI",
+            name: "SwiftUIContacts",
             dependencies: []),
         .testTarget(
-            name: "SwiftUIContactsUITests",
-            dependencies: ["SwiftUIContactsUI"]),
+            name: "SwiftUIContactsTests",
+            dependencies: ["SwiftUIContacts"]),
     ]
 )
--- Tests/SwiftUIContactsTests/SwiftUIContactsTests.swift
+++ Tests/SwiftUIContactsTests/SwiftUIContactsTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import SwiftUIContactsUI
+@testable import SwiftUIContacts
 
-final class SwiftUIContactsUITests: XCTestCase {
+final class SwiftUIContactsTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftUIContactsUI().text, "Hello, World!")
+//        XCTAssertEqual(SwiftUIContactsUI().text, "Hello, World!")
     }
 }
